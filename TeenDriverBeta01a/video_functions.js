9e62653d28149d1a675cbeea9ed8989b95a4d81e
Tak Igusa <tigusa@jhu.edu>
2018-10-27 09:42:51 -0400

    add phph

--- TeenDriverBeta01a/video_functions.js
+++ TeenDriverBeta01a/video_functions.js
@@ -84,10 +84,13 @@ function clearVideo(reset=true) {
 	count_misses(reset);
 }
 
+var videos_seen = [];
+
 function loadVideo(videoNumber)	{
 	document.getElementById('videoSourceWebm').setAttribute('src','videos/' + videoNumber + '.webm');
 	document.getElementById('videoSourceOgg').setAttribute('src','videos/' + videoNumber + '.ogg');
     document.getElementById("video").load();
+    videos_seen.push(videoNumber);
 	clearVideo();
 }
 
